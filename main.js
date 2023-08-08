0215dff8863a8e549759b26c76961ee66c1509aa
Mohitsingh Thakur <121103486+Mohitsingh-19@users.noreply.github.com>
2023-08-08 16:36:56 +0530

    Update main.js
    Changed the no. of cars to 1000 and sightly changed the mutatation difference value

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const ctx = canvas.getContext("2d");
 const road=new Road(canvas.width/2,canvas.width*0.9);
 const car = new Car(road.getLaneCenter(1),100,30,50,"AI");
 
-const N=1;
+const N=1000;
 const cars = generateCars(N); 
 let bestCar=cars[0];
 if(localStorage.getItem("bestBrain")){
@@ -14,7 +14,7 @@ if(localStorage.getItem("bestBrain")){
             localStorage.getItem("bestBrain")        
         );
         if(i!=0){
-            NeuralNetwork.mutate(cars[i].brain,0.15)
+            NeuralNetwork.mutate(cars[i].brain,0.125)
         }
     }
     
