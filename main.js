85d9f041b6ce5d90a9661e6840c1712ac318e73f
Mohitsingh Thakur <121103486+Moh1tsingh@users.noreply.github.com>
2023-08-10 10:13:58 +0530

    Changed N to 500

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const ctx = canvas.getContext("2d");
 const road=new Road(canvas.width/2,canvas.width*0.9);
 const car = new Car(road.getLaneCenter(1),100,30,50,"AI");
 
-const N=1000;
+const N=500;
 const cars = generateCars(N); 
 let bestCar=cars[0];
 if(localStorage.getItem("bestBrain")){
